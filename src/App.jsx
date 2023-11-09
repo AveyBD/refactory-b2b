24007152a9df244883ff41f38c2c4fb1e42ab6be
Debashish D. Dev <hello@debashish.dev>
2023-11-09 17:19:11 +0600

    Structure Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import JoinUs from "./components/JoinUs";
+import SuccessStory from "./components/SuccessStory";
+import WhyRefactory from "./components/WhyRefactory";
 
 function App() {
   return (
     <>
       <Hero />
+      <JoinUs />
+      <SuccessStory />
+      <WhyRefactory />
+      <Footer />
     </>
   );
 }
