5e41c2b6e32392e0ef8df7a667dd34944238ca59
Debashish D. Dev <hello@debashish.dev>
2023-11-12 13:32:22 +0600

    Router Sorted

--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Hero from './components/Hero.jsx';
-import WhyRefactory from './components/WhyRefactory.jsx';
-import SuccessStory from './components/SuccessStory.jsx';
-import JoinUs from './components/JoinUs.jsx';
+import "./index.css";
+import router from "./router/MainRouter";
 
+const combinedRoutes = [...router];
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Hero/>,
-  },
-  {
-    path: "/whyrefactory",
-    element: <WhyRefactory/>,
-  },
-  {
-    path: "/successstory",
-    element: <SuccessStory/>,
-  },
-  {
-    path: "/joinus",
-    element: <JoinUs/>,
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={combinedRoutes} />
   </React.StrictMode>
-)
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Hero from "../components/Hero";
+import WhyRefactory from "../components/WhyRefactory";
+import SuccessStory from "../components/SuccessStory";
+import JoinUs from "../components/JoinUs";
+
+export const Home = () => {
+  return (
+    <>
+      <Hero />
+      <WhyRefactory />
+      <SuccessStory />
+      <JoinUs />
+    </>
+  );
+};
